35f096a0b14ac401cba6e8c7cc164fd2c12b4e4a
Anne Epstein <ajepst@gmail.com>
2012-12-20 16:06:17 -0600

    add translator

---
+++ NamingStandardsTemplateTranslator/Translator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NamingStandardsTemplateTraslator.Models;
+
+namespace NamingStandardsTemplateTraslator
+{
+    public class Translator
+    {
+        public string Execute(NamingStandardsTemplate template, string shortName)
+        {
+            var pieces = shortName.Split('_');
+
+            return
+                pieces.Select(piece => template.Abbreviations.First(x => x.PhysicalText.ToUpper() == piece.ToUpper()).LogicalText)
+                      .Aggregate("", (current, translatedPiece) => current + translatedPiece);
+        }
+    }
+}
