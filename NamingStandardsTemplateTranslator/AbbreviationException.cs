28033fadf45e3c15e5008b43e9d35ab2dc048336
Alonso Robles <alonso@alonsorobles.com>
2013-01-15 16:06:01 -0600

    Added abbreviation exception to allow better error message when an exception occurs during abbreviation.

---
+++ NamingStandardsTemplateTranslator/AbbreviationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NamingStandardsTemplateTranslator
+{
+    public class AbbreviationException : Exception
+    {
+        public AbbreviationException()
+        {
+        }
+
+        public AbbreviationException(string message) : base(message)
+        {
+        }
+
+        public AbbreviationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
